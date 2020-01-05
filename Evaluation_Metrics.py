eae64ea9e8e9b7ec6be85c10fb9aa853f1b519fc
zacker150 <zacker150@hotmail.com>
2020-01-05 02:26:26 -0600

    Globalized Evaluation_Metrics.py

--- Evaluation_Metrics.py
+++ Evaluation_Metrics.py
@@ -28,15 +28,12 @@ import Features
 import tensorflow as tf
 import logging
 
-logger = logging.getLogger('root')
-
-config=configparser.ConfigParser()
-config.read('Config_file.ini')
+from utils import Globals
 
 def Confusion_matrix(y_test, y_predict):
 		confusion_matrix=sklearn.metrics.confusion_matrix(y_test, y_predict, labels=[0,1])
 		tn, fp, fn, tp=confusion_matrix.ravel()
-		logger.info("Confusion Matrix (TN, FP, FN, TP):({}, {}, {}, {})".format(tn, fp, fn, tp))
+		Globals.logger.info("Confusion Matrix (TN, FP, FN, TP):({}, {}, {}, {})".format(tn, fp, fn, tp))
 		return ([tn, fp, fn, tp])
 
 def Confusion_matrix2(y_test, y_predict):
@@ -46,36 +43,36 @@ def Confusion_matrix2(y_test, y_predict):
 			y_predict=y_predict.eval()
 		confusion_matrix=sklearn.metrics.confusion_matrix(y_test, y_predict)
 		tn, fp, fn, tp=confusion_matrix.ravel()
-		logger.info("Confusion Matrix (TN, FP, FN, TP):({}, {}, {}, {})".format(tn, fp, fn, tp))
+		Globals.logger.info("Confusion Matrix (TN, FP, FN, TP):({}, {}, {}, {})".format(tn, fp, fn, tp))
 
 
 def Matthews_corrcoef(y_test, y_predict):
 		Mcc=sklearn.metrics.matthews_corrcoef(y_test, y_predict)
-		logger.info("Matthews_CorrCoef: {}".format(Mcc))
+		Globals.logger.info("Matthews_CorrCoef: {}".format(Mcc))
 		return (Mcc)
 		#return Mcc
 
 def ROC_AUC(y_test, y_predict):
 		ROC_AUC=sklearn.metrics.roc_auc_score(y_test, y_predict)
-		logger.info("ROC_AUC: {}".format(ROC_AUC))
+		Globals.logger.info("ROC_AUC: {}".format(ROC_AUC))
 		return (ROC_AUC)
 		#return ROC_AUC
 
 def Precision(y_test, y_predict):
 		precision=sklearn.metrics.precision_score(y_test, y_predict)
-		logger.info("Precision: {}".format(precision))
+		Globals.logger.info("Precision: {}".format(precision))
 		return (precision)
 		#return precision
 
 def Recall(y_test, y_predict):
 		recall=sklearn.metrics.recall_score(y_test, y_predict)
-		logger.info("Recall: {}".format(recall))
+		Globals.logger.info("Recall: {}".format(recall))
 		return recall
 		#return Recall
 
 def F1_score(y_test, y_predict):
 		f1_score=sklearn.metrics.f1_score(y_test, y_predict)
-		logger.info("F1_score: {}".format(f1_score))
+		Globals.logger.info("F1_score: {}".format(f1_score))
 		return f1_score
 		#return F1_score
 
@@ -89,28 +86,28 @@ def Cross_validation(clf, X, y):
 		cv_results = cross_validate(clf, X, y, cv=10, scoring=scoring, verbose=1, n_jobs=1, return_train_score=False)
 		#scores = cross_validate(clf, X, y, cv=10, verbose=1, n_jobs=-1,)
 		#conf_mat = confusion_matrix(y, y_predict)
-		logger.info("10 fold Cross_Validation: {}".format(cv_results))
+		Globals.logger.info("10 fold Cross_Validation: {}".format(cv_results))
 
 def Homogenity(y_test,y_predict):
 		homogenity=sklearn.metrics.homogeneity_score(y_test,y_predict)
-		logger.info("Homogenity: {}".format(homogenity))
+		Globals.logger.info("Homogenity: {}".format(homogenity))
 
 def Completeness(y_test,y_predict):
 		completeness=sklearn.metrics.completeness_score(y_test,y_predict)
-		logger.info("Completeness: {}".format(completeness))
+		Globals.logger.info("Completeness: {}".format(completeness))
 
 def V_measure(y_test,y_predict):
 		v_measure=sklearn.metrics.v_measure_score(y_test,y_predict)
-		logger.info("V_measure: {}".format(v_measure))
+		Globals.logger.info("V_measure: {}".format(v_measure))
 
 def Geomteric_mean_score(y_test,y_predict):
 		g_mean=geometric_mean_score(y_test,y_predict)
-		logger.info("G_mean: {}".format(g_mean))
+		Globals.logger.info("G_mean: {}".format(g_mean))
 		return g_mean
 
 def Balanced_accuracy_score(y_test,y_predict):
 		b_accuracy=sklearn.metrics.balanced_accuracy_score(y_test,y_predict)
-		logger.info("Balanced_accuracy_score: {}".format(b_accuracy))
+		Globals.logger.info("Balanced_accuracy_score: {}".format(b_accuracy))
 		return b_accuracy
 
 def eval_metrics(clf, y_test, y_predict):
@@ -118,45 +115,45 @@ def eval_metrics(clf, y_test, y_predict):
 	summary.write("\n\nEvaluation metrics used:\n")
 	summary.write("\n\n Supervised metrics:\n")
 	eval_metrics_dict = {}
-	if config["Evaluation Metrics"]["Confusion_matrix"] == "True":
+	if Globals.config["Evaluation Metrics"]["Confusion_matrix"] == "True":
 		cm = Confusion_matrix(y_test, y_predict)
 		eval_metrics_dict['CM'] = cm
 		summary.write("Confusion_matrix\n")
-	if config["Evaluation Metrics"]["Matthews_corrcoef"] == "True":
+	if Globals.config["Evaluation Metrics"]["Matthews_corrcoef"] == "True":
 		mcc = Matthews_corrcoef(y_test, y_predict)
 		eval_metrics_dict['MCC'] = mcc
 		summary.write("Matthews_corrcoef\n")
-	if config["Evaluation Metrics"]["ROC_AUC"] == "True":
+	if Globals.config["Evaluation Metrics"]["ROC_AUC"] == "True":
 		roc_auc = ROC_AUC(y_test, y_predict)
 		eval_metrics_dict['ROC_AUC'] = roc_auc
 		summary.write("ROC_AUC\n")
-	if config["Evaluation Metrics"]["Precision"] == "True":
+	if Globals.config["Evaluation Metrics"]["Precision"] == "True":
 		precision = Precision(y_test, y_predict)
 		eval_metrics_dict['Precision'] = precision
 		summary.write("Precision\n")
-	if config["Evaluation Metrics"]["Recall"] == "True":
+	if Globals.config["Evaluation Metrics"]["Recall"] == "True":
 		recall = Recall(y_test, y_predict)
 		eval_metrics_dict['Recall'] = recall
 		summary.write("Recall\n")
-	if config["Evaluation Metrics"]["F1_score"] == "True":
+	if Globals.config["Evaluation Metrics"]["F1_score"] == "True":
 		f1_score = F1_score(y_test, y_predict)
 		eval_metrics_dict['F1_score'] = f1_score
 		summary.write("F1_score\n")
 	#if config["Evaluation Metrics"]["Cross_validation"] == "True":
 	#	Cross_validation(clf, X, y)
 	#	summary.write("Cross_validation\n")
-	if config["Evaluation Metrics"]["Geomteric_mean_score"] == "True":
+	if Globals.config["Evaluation Metrics"]["Geomteric_mean_score"] == "True":
 		gmean = Geomteric_mean_score(y_test,y_predict)
 		eval_metrics_dict['Gmean'] = gmean
 		summary.write("Geomteric_mean_score\n")
-	if config["Evaluation Metrics"]["Balanced_accuracy_score"] == "True":
+	if Globals.config["Evaluation Metrics"]["Balanced_accuracy_score"] == "True":
 		accuracy = Balanced_accuracy_score(y_test,y_predict)
 		eval_metrics_dict['accuracy'] = accuracy
 		summary.write("Balanced_accuracy_score\n")
 	#	# write results to summary
-	if config["Classification"]["Attack Features"] == "True":
-		logger.debug("Original Labels: {}".format(y_test))
-		logger.debug("New Labels: {}".format(y_predict))
+	if Globals.config["Classification"]["Attack Features"] == "True":
+		Globals.logger.debug("Original Labels: {}".format(y_test))
+		Globals.logger.debug("New Labels: {}".format(y_predict))
 	return (eval_metrics_dict)
 
 def eval_metrics_cluster(y_test, y_predict):
@@ -164,15 +161,15 @@ def eval_metrics_cluster(y_test, y_predict):
 	summary.write("\n\nEvaluation metrics used:\n")
 	summary.write("\n\n clustering metrics:\n")
 	eval_metrics_dict_cluster = {}
-	if config["Evaluation Metrics"]["Homogenity"] == "True":
+	if Globals.config["Evaluation Metrics"]["Homogenity"] == "True":
 		homogeneity_score = Homogenity(y_test, y_predict)
 		eval_metrics_dict_cluster['Homogenity'] = homogeneity_score
 		summary.write("Homogenity\n")
-	if config["Evaluation Metrics"]["Completeness"] == "True":
+	if Globals.config["Evaluation Metrics"]["Completeness"] == "True":
 		completeness_score = Completeness(y_test, y_predict)
 		eval_metrics_dict_cluster['Completeness'] = completeness_score
 		summary.write("Completeness\n")
-	if config["Evaluation Metrics"]["V_measure"] == "True":
+	if Globals.config["Evaluation Metrics"]["V_measure"] == "True":
 		v_measure_score = V_measure(y_test,y_predict)
 		eval_metrics_dict_cluster['V_measure'] = v_measure_score
 		summary.write("V_measure\n")
