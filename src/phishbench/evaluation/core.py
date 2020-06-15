ee288c772bf077738fe6674c7fe3f3df1831ead3
zacker150 <zacker150@hotmail.com>
2020-06-15 16:21:47 -0500

    Added classifier evaluation skeleton

---
+++ src/phishbench/evaluation/core.py
@@ -0,0 +1,6 @@
+from ..classification.core import BaseClassifier
+
+
+def evaluate_classifier(classifier: BaseClassifier, x_test, y_test):
+    #TODO Implement classifier evaluation
+    pass
---
+++ src/phishbench/evaluation/metrics.py
@@ -0,0 +1,4 @@
+from ..classification.core import BaseClassifier
+
+def accuracy(clf: BaseClassifier, x_test, y_test):
+    pass
