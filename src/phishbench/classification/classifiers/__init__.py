289da0c19e69864941c81f12e18239edc0d659ac
Radoslaw Konopka <47802853+radoslawkonopka@users.noreply.github.com>
2020-06-23 19:32:10 -0400

    I created Logistic Regression. (#102)
    * I created Logistic Regression.
    
    * Fixed name conflict in LogisticRegression
    
    Co-authored-by: zacker150 <zacker150@hotmail.com>

--- src/phishbench/classification/classifiers/__init__.py
+++ src/phishbench/classification/classifiers/__init__.py
@@ -4,3 +4,4 @@ from .k_nearest_neighbors import KNN
 from .multinomial_nb import MultinomialNaiveBayes
 from .random_forest import RandomForest
 from .svm import SVM
+from .logistic_regression import LogisticRegression
