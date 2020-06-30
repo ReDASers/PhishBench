6b1c84e9a36b6af08c74e8e727e6bbe2b6c58be7
zacker150 <zacker150@hotmail.com>
2020-06-29 23:25:46 -0500

    Incremented version number

--- setup.py
+++ setup.py
@@ -8,13 +8,12 @@ entry_points = {
 with open("requirements.txt") as req:
     reqs = req.readlines()
 
-
 setup(name='PhishBench',
-      version='0.1',
+      version='0.1.1',
       description='A Phishing detection benchmarking framework',
       url='https://github.com/sbaki2/Phishing-Detection',
       packages=find_packages("src"),
       package_dir={"": "src"},
-      package_data={"" : ["chromedriver"]},
+      package_data={"": ["chromedriver"]},
       install_requires=reqs,
       entry_points=entry_points)
