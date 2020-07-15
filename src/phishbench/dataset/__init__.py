f57afacb6d718e325093cfe4f85a4cdf0ecb715a
zacker150 <zacker150@hotmail.com>
2020-07-15 12:46:39 -0500

    Updated settings to point to an unlabeled dataset
    Added unlabeled path to settings
    
    Moved dataset path to settings file

--- src/phishbench/dataset/__init__.py
+++ src/phishbench/dataset/__init__.py
@@ -2,3 +2,4 @@ from .settings import test_legit_path
 from .settings import test_phish_path
 from .settings import train_legit_path
 from .settings import train_phish_path
+from .settings import unlabeled_path
