8a296109614de36865cd2d045263bdf7169f9287
Victor Zeng <zacker150@users.noreply.github.com>
2020-07-15 23:15:41 -0500

    Email unlabeled datasets (#129)
    * Added code for unlabeled email datasets to feature_extraction
    
    * Integrated unlabeled email extraction

--- src/phishbench/feature_extraction/email/__init__.py
+++ src/phishbench/feature_extraction/email/__init__.py
@@ -1,3 +1,3 @@
 from . import settings
-from .email_features import extract_labeled_dataset
+from .email_features import extract_labeled_dataset, extract_unlabeled_dataset
 from .reflection.core import FeatureType
--- src/phishbench/feature_extraction/email/email_features.py
+++ src/phishbench/feature_extraction/email/email_features.py
@@ -11,7 +11,7 @@ from ...input.email_input.models import EmailMessage
 from ...utils import phishbench_globals
 
 
-def extract_labeled_dataset(legit_dataset_folder, phish_dataset_folder):
+def extract_labeled_dataset(legit_dataset_folder: str, phish_dataset_folder: str):
     """
     Extracts features from a dataset of emails split in two folders
     :param legit_dataset_folder:
@@ -19,6 +19,12 @@ def extract_labeled_dataset(legit_dataset_folder, phish_dataset_folder):
     :param phish_dataset_folder:
         The folder containing emails of the legitimate class
     :return:
+    List[Dict]:
+        The extracted features
+    List[int]:
+        The labels of corresponding to the features
+    List[str]:
+        The corpus of emails
     """
     features = reflection.load_internal_features()
     print("Loaded {} features".format(len(features)))
@@ -41,6 +47,29 @@ def extract_labeled_dataset(legit_dataset_folder, phish_dataset_folder):
     corpus_train = legit_corpus + phish_corpus
 
     return feature_list_dict_train, labels_train, corpus_train
+
+
+def extract_unlabeled_dataset(dataset_folder: str):
+    """
+    Extracts features from a set of unlabeled urls
+    :param dataset_folder:
+        The folder containing the emails to extract features from
+    :return:
+    List[Dict]:
+        The extracted features
+    List[str]:
+        The corpus of emails
+    """
+    features = reflection.load_internal_features()
+    print("Loaded {} features".format(len(features)))
+
+    Globals.logger.info("Extracting unlabeled email features. %s", dataset_folder)
+
+    emails = pb_input.read_dataset_email(dataset_folder)
+
+    features, corpus = extract_email_features(emails, features)
+
+    return features, corpus
 
 
 def extract_email_features(emails: List[EmailMessage], features: List[Callable]):
