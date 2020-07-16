4f88a9044495dc591e82970c8463424b8097b8a2
zacker150 <zacker150@hotmail.com>
2020-07-15 19:05:03 -0500

    Refactored email feature extraction

--- src/phishbench/feature_extraction/email/email_features.py
+++ src/phishbench/feature_extraction/email/email_features.py
@@ -1,43 +1,47 @@
 import re
+from typing import List, Callable
 
-from phishbench import Features
-from phishbench.input import input as pb_input
-from phishbench.utils import Globals
 from . import reflection
+from ... import Features
+from ...input import input as pb_input
+from ...input.email_input.models import EmailMessage
+from ...utils import Globals
 
 
-def extract_dataset_features(legit_dataset_folder, phish_dataset_folder):
+def extract_labeled_dataset(legit_dataset_folder, phish_dataset_folder):
+    features = reflection.load_internal_features()
+    print("Loaded {} features".format(len(features)))
+
     Globals.logger.info("Extracting email features. Legit: %s Phish: %s", legit_dataset_folder, phish_dataset_folder)
 
-    legit_features, legit_corpus = extract_email_features(legit_dataset_folder)
-    phish_features, phish_corpus = extract_email_features(phish_dataset_folder)
+    print("Extracting Email features from %s", legit_dataset_folder)
+    legit_emails = pb_input.read_dataset_email(legit_dataset_folder)
+    legit_features, legit_corpus = extract_email_features(legit_emails, features)
+
+    print("Extracting Email features from %s", phish_dataset_folder)
+    phish_emails = pb_input.read_dataset_email(phish_dataset_folder)
+    phish_features, phish_corpus = extract_email_features(phish_emails, features)
 
     feature_list_dict_train = legit_features + phish_features
-
     labels_train = [0] * len(legit_features) + [1] * len(phish_features)
     corpus_train = legit_corpus + phish_corpus
+
     return feature_list_dict_train, labels_train, corpus_train
 
 
-def extract_email_features(dataset_path):
+def extract_email_features(emails: List[EmailMessage], features: List[Callable]):
     """
-    :param dataset_path:
-        The folder containing the datset
+    :param emails:
+        The dataset of emails
+    :param features:
+        A list of features to extract
     :return:
     List[Dict]:
         The extracted features
     List[str]:
         The corpus of emails
     """
-
-    print("Extracting Email features from %s", dataset_path)
-    Globals.logger.info("Extracting Email features from %s", dataset_path)
-
-    emails, _ = pb_input.read_dataset_email(dataset_path)
-
     feature_dict_list = list()
-
-    features = reflection.load_internal_features()
 
     for email_msg in emails:
         feature_values, _ = reflection.extract_features_from_single_email(features, email_msg)
@@ -57,7 +61,7 @@ def get_url(body):
 def email_url_features(url_list, list_features, list_time):
     if Globals.config["Email_Features"]["extract body features"] == "True":
         Globals.logger.debug("Extracting email URL features")
-        #Features.Email_URL_Number_Url(url_All, list_features, list_time)
+        # Features.Email_URL_Number_Url(url_All, list_features, list_time)
         Features.Email_URL_Number_Diff_Domain(url_list, list_features, list_time)
         Features.Email_URL_Number_link_at(url_list, list_features, list_time)
         Features.Email_URL_Number_link_sec_port(url_list, list_features, list_time)
