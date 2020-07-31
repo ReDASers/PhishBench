2282db88f00397272632b729bb6b80004db6804f
zacker150 <zacker150@hotmail.com>
2020-07-31 01:51:13 -0500

    Fixed line too long

--- src/phishbench/feature_extraction/email/email_features.py
+++ src/phishbench/feature_extraction/email/email_features.py
@@ -23,7 +23,8 @@ def extract_labeled_dataset(legit_dataset_folder, phish_dataset_folder):
     features = reflection.load_internal_features()
     print("Loaded {} features".format(len(features)))
 
-    phishbench_globals.logger.info("Extracting email features. Legit: %s Phish: %s", legit_dataset_folder, phish_dataset_folder)
+    phishbench_globals.logger.info("Extracting email features. Legit: %s Phish: %s",
+                                   legit_dataset_folder, phish_dataset_folder)
 
     print("Loading emails from {}".format(legit_dataset_folder))
     legit_emails, _ = pb_input.read_dataset_email(legit_dataset_folder)
