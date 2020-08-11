df61b6f95adcca1aa3aea9c6e290dbd35880d2bc
zacker150 <zacker150@hotmail.com>
2020-08-11 00:58:35 -0500

    Fixed integration test

--- tests/integration tests/make-config/features.py
+++ tests/integration tests/make-config/features.py
@@ -1,4 +1,4 @@
-from phishbench.feature_extraction.email.reflection import register_feature, FeatureType
+from phishbench.feature_extraction.reflection import register_feature, FeatureType
 
 
 @register_feature(FeatureType.EMAIL_HEADER, "test_feature")
