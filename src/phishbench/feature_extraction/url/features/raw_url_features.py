74db41643cff8262a5321c3ecbcb85af0ce06d19
zacker150 <zacker150@hotmail.com>
2020-08-19 03:02:06 -0500

    Added docstrings

--- src/phishbench/feature_extraction/url/features/raw_url_features.py
+++ src/phishbench/feature_extraction/url/features/raw_url_features.py
@@ -14,31 +14,49 @@ from ....input import URLData
 
 @register_feature(FeatureType.URL_RAW, 'domain_length')
 def domain_length(url: URLData):
+    """
+    The length of the domain of the url
+    """
     return len(url.parsed_url.hostname)
 
 
 @register_feature(FeatureType.URL_RAW, 'number_of_digits')
 def num_digits(url: URLData):
+    """
+    The number of digits in the url
+    """
     return sum(c.isdigit() for c in url.raw_url)
 
 
 @register_feature(FeatureType.URL_RAW, 'number_of_dots')
 def num_dots(url: URLData):
+    """
+    The number of times the `.` character occurs in the url
+    """
     return url.raw_url.count('.')
 
 
 @register_feature(FeatureType.URL_RAW, 'url_length')
 def url_length(url: URLData):
+    """
+    The length of the url.
+    """
     return len(url.raw_url)
 
 
 @register_feature(FeatureType.URL_RAW, 'special_pattern')
 def special_pattern(url: URLData):
+    """
+    Whether or not the string `?gws_rd=ssl` appears in the url
+    """
     return "?gws_rd=ssl" in url.raw_url
 
 
 @register_feature(FeatureType.URL_RAW, 'is_common_tld')
 def is_common_tld(url: URLData):
+    """
+    Whether or not the tld is one of: .com, .net, .org, .edu, .mil, .gov, .co, .biz", .info, .me
+    """
     common_tld_list = ["com", "net", "org", "edu", "mil", "gov", "co", "biz", "info", "me"]
     tld = tldextract.extract(url.raw_url).suffix
     return tld in common_tld_list
@@ -46,48 +64,75 @@ def is_common_tld(url: URLData):
 
 @register_feature(FeatureType.URL_RAW, 'is_ip_addr')
 def is_ip_addr(url: URLData):
+    """
+    Whether or not the url is an IPv4 address
+    """
     match = re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", url.parsed_url.hostname)
     return match is not None
 
 
 @register_feature(FeatureType.URL_RAW, 'has_https')
 def has_https(url: URLData):
+    """
+    Whether or not the url is https
+    """
     return url.parsed_url.scheme.startswith('https')
 
 
 @register_feature(FeatureType.URL_RAW, 'has_at_symbol')
 def has_at_symbol(url: URLData):
+    """
+    Whether or not the character `@` is in the url
+    """
     return '@' in url.raw_url
 
 
 @register_feature(FeatureType.URL_RAW, 'null_in_domain')
 def null_in_domain(url: URLData):
+    """
+    Whether or not the string `null` is in the url (ignoring case)
+    """
     return 'null' in url.parsed_url.hostname.lower()
 
 
 @register_feature(FeatureType.URL_RAW, 'number_of_dashes')
 def number_of_dashes(url: URLData):
+    """
+    The number of dashes in the url
+    """
     return url.raw_url.count('-')
 
 
 @register_feature(FeatureType.URL_RAW, 'number_of_slashes')
 def number_of_slashes(url: URLData):
+    """
+    The number forward or backward slashes in the url
+    """
     return url.raw_url.count('/') + url.raw_url.count('\\')
 
 
 @register_feature(FeatureType.URL_RAW, 'http_in_middle')
 def http_in_middle(url: URLData):
+    """
+    Whether or not the string 'http' occurs in the middle of the url.
+    """
     match = re.match(".+http.+", url.raw_url)
     return match is not None
 
 
 @register_feature(FeatureType.URL_RAW, 'has_port')
 def has_port(url: URLData):
+    """
+    Whether or not the URL has a port number
+    """
     return url.parsed_url.port is not None
 
 
 @register_feature(FeatureType.URL_RAW, 'num_punctuation')
 def num_punctuation(url: URLData):
+    """
+    Number of punctuation. Punctuation is defined as `string.punctuation`
+    """
     return sum(x in string.punctuation for x in url.raw_url)
 
 
