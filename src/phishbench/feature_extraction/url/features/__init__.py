4083ef3965a95abd153646cdd3c90b7215147579
zacker150 <zacker150@hotmail.com>
2020-09-03 18:45:00 -0500

    Empty file for network features

--- src/phishbench/feature_extraction/url/features/__init__.py
+++ src/phishbench/feature_extraction/url/features/__init__.py
@@ -1,6 +1,7 @@
 """
 This module contains the built-in URL/website features
 """
+from .network_features import *
 from .raw_url_features import *
 from .url_token_features import *
 from .url_whitelist_features import *
---
+++ src/phishbench/feature_extraction/url/features/network_features.py
@@ -0,0 +1,3 @@
+"""
+This module contains the built-in network features
+"""
