4304775a1ef15ef729ba6b5783e24fa2e50b57fc
Victor Zeng <zacker150@users.noreply.github.com>
2020-09-10 10:05:12 -0500

    Added import for l-tree features (#206)
    * Added import to features
    
    * Fixed import in unit test

--- src/phishbench/feature_extraction/url/features/__init__.py
+++ src/phishbench/feature_extraction/url/features/__init__.py
@@ -1,6 +1,7 @@
 """
 This module contains the built-in URL/website features
 """
+from .link_tree_features import link_tree_features
 from .network_features import *
 from .raw_url_features import *
 from .url_token_features import *
--- tests/unit_tests/features/url/test_link_tree_features.py
+++ tests/unit_tests/features/url/test_link_tree_features.py
@@ -1,10 +1,10 @@
 """
-Tests for network features
+Tests for l-tree features
 """
 import unittest
 
 from phishbench.input import URLData
-from phishbench.feature_extraction.url.features import link_tree_features
+from phishbench.feature_extraction.url import features
 from tests import mock_objects
 
 # pylint: disable=missing-function-docstring
@@ -17,6 +17,6 @@ class TestLinkTreeFeatures(unittest.TestCase):
         url: URLData = mock_objects.get_mock_object("wikipedia_shortener_urldata")
         expected = mock_objects.get_mock_object('wikipedia-Ltree')
 
-        result = link_tree_features.link_tree_features(url)
+        result = features.link_tree_features(url)
 
         self.assertDictEqual(expected, result)
