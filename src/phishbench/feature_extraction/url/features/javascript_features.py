b46a9062e66b153af6a92005db044b64e18a5e91
zacker150 <zacker150@hotmail.com>
2020-09-12 17:50:14 -0500

    JavaScript features

---
+++ src/phishbench/feature_extraction/url/features/javascript_features.py
@@ -0,0 +1,104 @@
+"""
+This module contains built-in javascript features
+"""
+import re
+from bs4 import BeautifulSoup
+
+from ...reflection import register_feature, FeatureType
+from ....input.url_input import URLData
+
+
+@register_feature(FeatureType.URL_WEBSITE_JAVASCRIPT, 'number_of_exec')
+def number_of_exec(url: URLData):
+    """
+    Number of `exec` calls in the embedded javascript
+    """
+    soup = BeautifulSoup(url.downloaded_website, 'html5lib')
+    scripts = [script for script in soup.find_all('script')
+               if script.get("type") is None or script.get("type") == 'text/javascript']
+    counts = [len(re.findall(r'\Wexec\(', str(script))) for script in scripts]
+    return sum(counts)
+
+
+@register_feature(FeatureType.URL_WEBSITE_JAVASCRIPT, 'number_of_escape')
+def number_of_escape(url: URLData):
+    """
+    Number of `escape` calls in the embedded javascript
+    """
+    soup = BeautifulSoup(url.downloaded_website, 'html5lib')
+    scripts = [script for script in soup.find_all('script')
+               if script.get("type") is None or script.get("type") == 'text/javascript']
+    counts = [len(re.findall(r'\Wescape\(', str(script))) for script in scripts]
+    return sum(counts)
+
+
+@register_feature(FeatureType.URL_WEBSITE_JAVASCRIPT, 'number_of_eval')
+def number_of_eval(url: URLData):
+    """
+    Number of `eval` calls in the embedded javascript
+    """
+    soup = BeautifulSoup(url.downloaded_website, 'html5lib')
+    scripts = [script for script in soup.find_all('script')
+               if script.get("type") is None or script.get("type") == 'text/javascript']
+    counts = [len(re.findall(r'\Weval\(', str(script))) for script in scripts]
+    return sum(counts)
+
+
+@register_feature(FeatureType.URL_WEBSITE_JAVASCRIPT, 'number_of_link')
+def number_of_link(url: URLData):
+    """
+    Number of `link` calls in the embedded javascript
+    """
+    soup = BeautifulSoup(url.downloaded_website, 'html5lib')
+    scripts = [script for script in soup.find_all('script')
+               if script.get("type") is None or script.get("type") == 'text/javascript']
+    counts = [len(re.findall(r'\Wlink\(', str(script))) for script in scripts]
+    return sum(counts)
+
+
+@register_feature(FeatureType.URL_WEBSITE_JAVASCRIPT, 'number_of_unescape')
+def number_of_unescape(url: URLData):
+    """
+    Number of `unescape` calls in the embedded javascript
+    """
+    soup = BeautifulSoup(url.downloaded_website, 'html5lib')
+    scripts = [script for script in soup.find_all('script')
+               if script.get("type") is None or script.get("type") == 'text/javascript']
+    counts = [len(re.findall(r'\Wunescape\(', str(script))) for script in scripts]
+    return sum(counts)
+
+
+@register_feature(FeatureType.URL_WEBSITE_JAVASCRIPT, 'number_of_search')
+def number_of_search(url: URLData):
+    """
+    Number of `search` calls in the embedded javascript
+    """
+    soup = BeautifulSoup(url.downloaded_website, 'html5lib')
+    scripts = [script for script in soup.find_all('script')
+               if script.get("type") is None or script.get("type") == 'text/javascript']
+    counts = [len(re.findall(r'\Wsearch\(', str(script))) for script in scripts]
+    return sum(counts)
+
+
+@register_feature(FeatureType.URL_WEBSITE_JAVASCRIPT, 'number_of_set_timeout')
+def number_of_set_timeout(url: URLData):
+    """
+    Number of `setTimeout` calls in the embedded javascript
+    """
+    soup = BeautifulSoup(url.downloaded_website, 'html5lib')
+    scripts = [script for script in soup.find_all('script')
+               if script.get("type") is None or script.get("type") == 'text/javascript']
+    counts = [len(re.findall(r'\WsetTimeout\(', str(script))) for script in scripts]
+    return sum(counts)
+
+
+@register_feature(FeatureType.URL_WEBSITE_JAVASCRIPT, 'number_of_iframes_in_script')
+def number_of_iframes_in_script(url: URLData):
+    """
+    Number of times the token `iframe` shows up in embedded javascript
+    """
+    soup = BeautifulSoup(url.downloaded_website, 'html5lib')
+    scripts = [script for script in soup.find_all('script')
+               if script.get("type") is None or script.get("type") == 'text/javascript']
+    counts = [len(re.findall(r'iframe', str(script))) for script in scripts]
+    return sum(counts)
---
+++ tests/unit_tests/features/url/test_javascript_features.py
@@ -0,0 +1,61 @@
+"""
+Tests JavaScript features
+"""
+import unittest
+
+from phishbench.feature_extraction.url.features import javascript_features
+from tests import mock_objects
+
+
+# pylint: disable=missing-function-docstring
+# pylint: disable=too-many-public-methods
+
+
+class TesJavaScriptReflectionFeatures(unittest.TestCase):
+    """
+    Tests javascript_features
+    """
+
+    def test_num_exec(self):
+        test_url = mock_objects.get_mock_object('regex_tester_urldata.pkl')
+
+        result = javascript_features.number_of_exec(test_url)
+        self.assertEqual(1, result)
+
+    def test_num_escape(self):
+        test_url = mock_objects.get_mock_object('escape_urldata.pkl')
+        result = javascript_features.number_of_escape(test_url)
+        self.assertEqual(2, result)
+
+    def test_number_of_eval(self):
+        test_url = mock_objects.get_mock_object('escape_urldata.pkl')
+        result = javascript_features.number_of_eval(test_url)
+        self.assertEqual(6, result)
+
+    def test_number_of_link(self):
+        test_url = mock_objects.get_mock_object('escape_urldata.pkl')
+        result = javascript_features.number_of_link(test_url)
+        self.assertEqual(3, result)
+
+    def test_num_unescape(self):
+        test_url = mock_objects.get_mock_object('escape_urldata.pkl')
+        result = javascript_features.number_of_unescape(test_url)
+        self.assertEqual(1, result)
+
+    def test_num_search(self):
+        test_url = mock_objects.get_mock_object('escape_urldata.pkl')
+        result = javascript_features.number_of_search(test_url)
+        self.assertEqual(2, result)
+
+    def test_num_set_timeout(self):
+        test_url = mock_objects.get_mock_object('escape_urldata.pkl')
+        result = javascript_features.number_of_set_timeout(test_url)
+        self.assertEqual(4, result)
+
+    def test_num_iframe(self):
+        test_url = mock_objects.get_mock_object('escape_urldata.pkl')
+        result = javascript_features.number_of_iframes_in_script(test_url)
+        self.assertEqual(2, result)
+
+
+
