0b0cc7e77755349b2d4aaa0400b807ea35c9a8be
zacker150 <zacker150@hotmail.com>
2020-09-22 21:34:40 -0500

    Documentation for classification.settings

--- src/phishbench/classification/settings.py
+++ src/phishbench/classification/settings.py
@@ -1,3 +1,6 @@
+"""
+Settings for the classification module
+"""
 from ..utils import phishbench_globals
 
 CLASSIFICATION_SECTION = 'Classification'
@@ -13,23 +16,46 @@ DEFAULT_SETTINGS = {
 }
 
 
-def load_models():
+def load_models() -> bool:
+    """
+    Whether or not to load the models from disk
+    """
     return phishbench_globals.config[CLASSIFICATION_SECTION].getboolean("load models")
 
 
-def save_models():
+def save_models() -> bool:
+    """
+    Whether or not to save the models to disk
+    """
     return phishbench_globals.config[CLASSIFICATION_SECTION].getboolean("save models")
 
 
-def param_search():
+def param_search() -> bool:
+    """
+    Whether or not to perform parameter search
+    """
     return phishbench_globals.config[CLASSIFICATION_SECTION].getboolean("param search")
 
 
-def weighted_training():
+def weighted_training() -> bool:
+    """
+    Whether or not to perform weighted training
+    """
     return phishbench_globals.config[CLASSIFICATION_SECTION].getboolean("weighted")
 
 
-def is_enabled(classifier: type):
+def is_enabled(classifier: type) -> bool:
+    """
+    Whether or not a classifier is enabled
+    Parameters
+    ----------
+    classifier:
+        The classifier class to check
+
+    Returns
+    -------
+        Whether or not the classifier is enabled
+    """
     return phishbench_globals.config[CLASSIFIERS_SECTION].getboolean(classifier.__name__)
 
 # def num_rounds() -> int:
