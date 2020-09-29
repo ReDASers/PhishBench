5645526c2cba215f3c9f2960257781008d8792eb
zacker150 <zacker150@hotmail.com>
2020-09-28 22:01:01 -0500

    Removed TF-IDF hack from main email body selection

--- src/phishbench/feature_preprocessing/_cleaning.py
+++ src/phishbench/feature_preprocessing/_cleaning.py
@@ -1,6 +1,8 @@
 """
 This module contains feature cleaning code
 """
+import numpy as np
+import scipy.sparse
 from typing import List, Dict
 
 from ..utils import phishbench_globals
@@ -17,6 +19,8 @@ def clean_features(feature_values: List[Dict]):
     phishbench_globals.logger.debug('Cleaning')
     for feature_dict in feature_values:
         for key, value in feature_dict.items():
+            if isinstance(value, np.ndarray) or scipy.sparse.issparse(value):
+                continue
             if value in ["None", "N/A", "NaN", None]:
                 feature_dict[key] = -1
                 phishbench_globals.logger.debug("Value of %s changed from %s to -1", key, value)
