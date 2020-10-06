183be5b7aeeadf7ba292dd921731a660cab9af2b
zacker150 <zacker150@hotmail.com>
2020-10-06 17:21:35 -0500

    Lint

--- tests/unit_tests/input/email_input/test_email_header.py
+++ tests/unit_tests/input/email_input/test_email_header.py
@@ -1,12 +1,20 @@
+"""
+Tests handling of email headers
+"""
 import unittest
 from unittest.mock import patch
 
 from phishbench.input.email_input.models import EmailHeader
 from phishbench.input.email_input.models._header import parse_address_list
-from tests.sample_emails.utils import get_binary_email
+from tests.sample_emails.utils import get_binary_email  # pylint: disable=import-error, no-name-in-module
 
 
 class TestEmailHeader(unittest.TestCase):
+    """
+    Tests `models.EmailHeader`
+    """
+    # pylint: disable=missing-function-docstring
+    # pylint: disable=too-many-public-methods
 
     def test_parse_address_list_none(self):
         result = parse_address_list(None)
@@ -48,7 +56,7 @@ class TestEmailHeader(unittest.TestCase):
         self.assertEqual(8, date.minute)
         self.assertEqual(50, date.second)
 
-    def test_X_priority(self):
+    def test_x_priority(self):
         msg = get_binary_email("HeaderTests/Test Email 2.txt")
 
         header = EmailHeader(msg)
@@ -56,7 +64,7 @@ class TestEmailHeader(unittest.TestCase):
         self.assertEqual(1, header.x_priority)
 
     @patch('phishbench.utils.phishbench_globals.logger.debug')
-    def test_X_priority_error(self, l_mock):
+    def test_x_priority_error(self, l_mock):
         msg = get_binary_email("HeaderTests/Test Email 2.txt")
         del msg['X-Priority']
         msg['X-Priority'] = "BAD"
@@ -66,7 +74,7 @@ class TestEmailHeader(unittest.TestCase):
         self.assertIsNone(header.x_priority)
         l_mock.assert_called()
 
-    def test_X_priority_null(self):
+    def test_x_priority_null(self):
         msg = get_binary_email("HeaderTests/Test Email 1.txt")
         header = EmailHeader(msg)
         self.assertIsNone(header.x_priority)
