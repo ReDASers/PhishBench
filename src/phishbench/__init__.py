5215dc165bc27e47d0789703e4f555deaaf1e2c0
zacker150 <zacker150@hotmail.com>
2020-10-10 02:27:33 -0500

    Version number

--- src/phishbench/__init__.py
+++ src/phishbench/__init__.py
@@ -3,5 +3,5 @@ PhishBench is an extendable benchmarking framework for Phishing Detection
 """
 from .utils.phishbench_globals import initialize
 
-version_info = (1, 2, 2)
+version_info = (1, 3, 0)
 __version__ = '.'.join(str(c) for c in version_info)
