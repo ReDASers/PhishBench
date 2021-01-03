32c007b753275482680c9aeaacf1e36207cab090
Victor Zeng <zacker150@users.noreply.github.com>
2021-01-02 21:47:29 -0600

    Added check for single class sample (#280)
    * Added check for single class sample
    
    * Fixed lint error

--- src/phishbench/feature_preprocessing/_preproccessing.py
+++ src/phishbench/feature_preprocessing/_preproccessing.py
@@ -3,11 +3,13 @@ Contains code to run preprocessing
 """
 import os
 
+import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 
-from . import settings
 from . import balancing
 from . import feature_selection
+from . import settings
+from ..utils import phishbench_globals
 
 
 def process_vectorized_features(x_train, y_train, x_test, feature_names, output_dir: str):
@@ -60,6 +62,10 @@ def process_vectorized_features(x_train, y_train, x_test, feature_names, output_
     y_train_dict = {}
     for balancing_method in train_samples:
         x_train, y_train = train_samples[balancing_method]
+        if len(np.unique(y_train)) == 1:
+            print(f"{balancing_method} produced samples with only one class. Omitting")
+            phishbench_globals.logger.warning("%s produced samples with only one class. Omitting", balancing_method)
+            continue
         y_train_dict[balancing_method] = y_train
         # Feature Selection
         if settings.feature_selection():
