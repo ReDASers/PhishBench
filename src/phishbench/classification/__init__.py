a35cadbac64e056596ab581a023586a7c50eae1e
Victor Zeng <zacker150@hotmail.com>
2021-01-14 19:18:55 -0600

    Exported load_classifier functions

--- src/phishbench/classification/__init__.py
+++ src/phishbench/classification/__init__.py
@@ -1,3 +1,3 @@
 from . import settings
 from .base_classifier import BaseClassifier
-from .core import train_classifiers
+from .core import train_classifiers, load_classifiers_from_module, load_classifiers
