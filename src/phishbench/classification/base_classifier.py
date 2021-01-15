aeb9bc9f275512ca0dffd92fabd69b20033ccb31
Victor Zeng <zacker150@hotmail.com>
2021-01-14 18:57:14 -0600

    Improved BaseClassifier docstring

--- src/phishbench/classification/base_classifier.py
+++ src/phishbench/classification/base_classifier.py
@@ -13,9 +13,9 @@ class BaseClassifier:
 
         Parameters
         ----------
-        io_dir: str
+        io_dir : str
             The folder to save this classifier to.
-        save_file:
+        save_file : str
             The file to save this classifier to
         """
         self.io_dir = io_dir
@@ -24,7 +24,7 @@ class BaseClassifier:
 
     def fit(self, x, y):
         """
-        Trains the classifier.
+        Trains the classifier on the provided dataset.
 
         If being used as a wrapper for a scikit-learn style classifier, then implementations of
         this function can simply store the trained underlying classifier in `self.clf`. Other implementations should
@@ -32,9 +32,9 @@ class BaseClassifier:
 
         Parameters
         ----------
-        x: array-like or sparse matrix of shape (n,f)
-            Training vectors, where n is the number of samples and f is the number of features.
-        y: array-like of shape (n)
+        x : array-like or sparse matrix of shape (n,f)
+            Training feature vectors, where n is the number of samples and f is the number of features.
+        y : array-like of shape (n)
             Target values, with 0 being legitimate and 1 being phishing
         """
 
@@ -49,9 +49,9 @@ class BaseClassifier:
 
         Parameters
         ----------
-        x: array-like or sparse matrix of shape (n,f)
-            Training vectors, where n is the number of samples and f is the number of features.
-        y: array-like of shape (n)
+        x : array-like or sparse matrix of shape (n,f)
+            Training feature vectors, where n is the number of samples and f is the number of features.
+        y : array-like of shape (n)
             Target values, with 0 being legitimate and 1 being phishing
         """
         print("{} does not support weighted training. Performing regular training.".format(self.name))
@@ -63,14 +63,14 @@ class BaseClassifier:
 
         Parameters
         ----------
-        x: array-like or sparse matrix of shape (n,f)
-            Training vectors, where n is the number of samples and f is the number of features.
-        y: array-like of shape (n)
+        x : array-like or sparse matrix of shape (n,f)
+            Training feature vectors, where n is the number of samples and f is the number of features.
+        y : array-like of shape (n)
             Target values, with 0 being legitimate and 1 being phishing
 
         Returns
         -------
-        dict:
+        dict
             The best parameters.
         """
         print("{} does not support parameter search. Performing regular training.".format(self.name))
@@ -78,11 +78,11 @@ class BaseClassifier:
 
     def predict(self, x):
         """
-        Generates a batch of classifications for the given input
+        Perform classification on `x`
 
         Parameters
         ----------
-        x: array-like or sparse matrix of shape (n,f)
+        x : array-like or sparse matrix of shape (n,f)
             Test vectors, where n is the number of samples and f is the number of features
 
         Returns
@@ -95,11 +95,11 @@ class BaseClassifier:
 
     def predict_proba(self, x):
         """
-        Generates a batch of probabilistic predictions for the given input
+        Estimates the probability of samples in X being phishing.
 
         Parameters
         ----------
-        x: array-like or sparse matrix of shape (n,f)
+        x : array-like or sparse matrix of shape (n,f)
             Test vectors, where n is the number of samples and f is the number of features
 
         Returns
@@ -112,13 +112,13 @@ class BaseClassifier:
 
     def load_model(self):
         """
-        Loads the model from `self.io_dir`
+        Loads the model from `self.model_path`
         """
         self.clf = joblib.load(self.model_path)
 
     def save_model(self):
         """
-        Saves the model to `self.io_dir`
+        Saves the model to `self.model_path`
         """
         assert self.clf is not None, "Classifier must be trained first"
         joblib.dump(self.clf, self.model_path)
