468c00c9767462dd26707878d0a2142db5159ff3
Victor Zeng <zacker150@hotmail.com>
2021-01-14 21:43:50 -0600

    Docstring for module

--- src/phishbench/classification/__init__.py
+++ src/phishbench/classification/__init__.py
@@ -1,5 +1,8 @@
 """
+This module handles the training of classifiers.
 
+Built in classifiers can be found in the `classifiers` subpackage. Users can write custom classifiers by subclassing
+the `BaseClassifier` class.
 """
 from . import settings
 from ._base_classifier import BaseClassifier
