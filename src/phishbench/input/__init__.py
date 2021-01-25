1cc22c0d179bc3ddf52bd322c6ac9764b7e4d247
Victor Zeng <zacker150@hotmail.com>
2021-01-25 02:02:06 -0600

    Longer input documentation

--- src/phishbench/input/__init__.py
+++ src/phishbench/input/__init__.py
@@ -1,7 +1,9 @@
 """
-This module loads datasets from raw emails and URLs. 
+This module loads datasets from raw emails and URLs. It is split into two sub-modules. The `email_input` submodule
+handles email datasets, and the `url_input` submodule handles URL datasets.
 
-
+In addition, this module contains `read_train_set` and `read_test_set` functions which uses the relevant submodule to
+read in datasets according to the global configuration.
 """
 from . import settings
 from ._input import read_test_set, read_train_set
