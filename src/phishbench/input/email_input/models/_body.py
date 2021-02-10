52e19f46767c0cd95cf4511954aff0d1006c93de
Victor Zeng <zacker150@hotmail.com>
2021-02-10 02:47:34 -0600

    disabled false Cleaner not found

--- src/phishbench/input/email_input/models/_body.py
+++ src/phishbench/input/email_input/models/_body.py
@@ -74,6 +74,7 @@ def decode_text_part(part):
 
 def clean_html(raw_html: str):
     assert isinstance(raw_html, str)
+    # pylint: disable=no-name-in-module
     cleaner = Cleaner()
     # We only want to remove javascript and style
     cleaner.javascript = True
